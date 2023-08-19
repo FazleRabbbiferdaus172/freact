9100dcfcda1f723b9d0bc43b0f2bf334e4a1f99b
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-08-19 22:28:01 +0600

    React.createElement is replaced

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const element = React.createElement(
-    "h1",
-    {
-        title: "freact"
+// React.createElement creates an object from its arguments. Besides some validations, that’s all it does. So we will replace the function call with its output 
+const element = {
+    type: "h1",
+    props: {
+        title: "freact",
+        children: "Hello Freact",
     },
-    "Hello Freact"
-)
-const container = document.getElementById("app")
-ReactDOM.render(element, container)
+};
+const container = document.getElementById("app");
+ReactDOM.render(element, container);
