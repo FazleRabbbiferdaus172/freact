330d458bf68675b4ede18a7013766433b7695222
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-08-19 21:40:11 +0600

    adding react to source to, this should result in an error as jsx is not valid jsgit add src/index.js git add src/index.js git add src/index.js git add src/index.js !

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("hello Freact")
+console.log("hello world")
+const element = <h1 title="foo">Hello</h1>
+const container = document.getElementById("app")
+ReactDOM.render(element, container)
