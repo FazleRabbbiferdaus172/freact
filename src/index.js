5edbf23ececfda53a96b7678904136c9f5a7c86b
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-08-20 00:51:19 +0600

    createElement and jsx is implemented

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// would be Freact.createElement(), returns just a object as expected from react.createElement
+// [v.2] would be Freact.createElement(), returns just a object as expected from react.createElement. also createTextElement is implemented for the childen those are primitive type
 function createElement(type, props, ...children) {
   return {
     type,
@@ -25,8 +25,11 @@ const Freact = {
   createElement,
 };
 
-// v.1 React.createElement creates an object from its arguments. Besides some validations, that’s all it does. So we will replace the function call with its output
-// Now replacing v.1 version of js to calling Freact.createElement() with jsx
+// End of Freact library
+
+
+// [v.1] React.createElement creates an object from its arguments. Besides some validations, that’s all it does. So we will replace the function call with its output
+// [v.2] Now replacing v.1 version of js to calling Freact.createElement() with jsx
 
 /** @jsx Didact.createElement */
 const element = (
@@ -40,11 +43,14 @@ const element = (
 
 const container = document.getElementById("app");
 
-// Now we replace ReactDOM.render() function
-const node = document.createElement(element.type);
-node["title"] = element.props.title;
-const children = document.createTextNode("");
-children["nodeValue"] = element.props.children;
-node.appendChild(children);
-container.appendChild(node);
+// [v.1] Now we replace ReactDOM.render() function
+// [v.2] Now calling Freact.render instead of ReactDOM.render
+
+// const node = document.createElement(element.type);
+// node["title"] = element.props.title;
+// const children = document.createTextNode("");
+// children["nodeValue"] = element.props.children;
+// node.appendChild(children);
+// container.appendChild(node);
+
 ////////////// ReactDOM.render() replaced
