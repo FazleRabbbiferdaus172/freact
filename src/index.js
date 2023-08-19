153fb4501bc123b04f2780dc644cb5880d787c98
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-08-20 00:49:15 +0600

    Implenting our own Freact library, createElement is implemented with jsx

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
-// React.createElement creates an object from its arguments. Besides some validations, that’s all it does. So we will replace the function call with its output
-const element = {
-  type: "h1",
-  props: {
-    title: "freact",
-    children: "Hello Freact",
-  },
+// would be Freact.createElement(), returns just a object as expected from react.createElement
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const Freact = {
+  createElement,
 };
+
+// v.1 React.createElement creates an object from its arguments. Besides some validations, that’s all it does. So we will replace the function call with its output
+// Now replacing v.1 version of js to calling Freact.createElement() with jsx
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="freact">
+    <h1>Hello Freact</h1>
+    <h2 />
+  </div>
+);
+
 ////////////// React.createElement replaced
 
 const container = document.getElementById("app");
