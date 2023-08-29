142fabf99965e87f5f95a7e70350c3eedf9cc1d9
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-08-30 01:16:35 +0600

    useState implmented, still issue remain is that there is no click event

--- src/index.js
+++ src/index.js
@@ -110,9 +110,48 @@ function performUnitOfWork(fiber) {
   }
 }
 
+let wipFiber = null;
+let hookIndex = null;
+
 function updateFunctionComponent(fiber) {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hooks = [];
   const children = [fiber.type(fiber.props)];
   reconcileChildren(fiber, children);
+}
+
+function useState(initial) {
+  // implement useState
+  const oldHook = wipFiber.alternate && 
+    wipFiber.alternate.hooks &&
+    wipFiber.alternate.hooks &&
+    wipFiber.alternate.hook[hookIndex];
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  };
+
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach(
+    action => {
+      hook.state = action(hook.state);
+    }
+  );
+
+  const setState = action => {
+    hook.queue.push(action);
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    };
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  };
+  wipFiber.hooks.push(hook);
+  hookIndex++;
+  return [hook.state, setState];
 }
 
 function updateHostcomponent(fiber) {
@@ -291,7 +330,7 @@ const element = (
 function AppFunctionComponent() {
   const [state, setState] = Freact.useState(1);
   return (
-    <h1 onClick={() => setState(c => c +1 )}>Freact Counts, {state}</h1>
+    <h1 onClick={() => setState(c => c +1)}>Freact Counts, {state}</h1>
   );
 }
 
